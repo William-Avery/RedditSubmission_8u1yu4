4b9e48562290414f6cb1f20e8eefef82be5400e9
William Avery <willaveryvy@gmail.com>
2018-06-27 02:01:38 -0400

    Comit

--- Submission/Form1.Designer.cs
+++ Submission/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.b_generate = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -37,11 +38,20 @@
             this.label3 = new System.Windows.Forms.Label();
             this.tb_supemail = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.adobebrowser = new AxAcroPDFLib.AxAcroPDF();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.adobebrowser)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // b_generate
             // 
-            this.b_generate.Location = new System.Drawing.Point(110, 138);
+            this.b_generate.Location = new System.Drawing.Point(6, 133);
             this.b_generate.Name = "b_generate";
             this.b_generate.Size = new System.Drawing.Size(87, 25);
             this.b_generate.TabIndex = 0;
@@ -52,7 +62,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 12);
+            this.label1.Location = new System.Drawing.Point(22, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(80, 14);
             this.label1.TabIndex = 1;
@@ -61,7 +71,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Location = new System.Drawing.Point(10, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 14);
             this.label2.TabIndex = 2;
@@ -69,29 +79,29 @@
             // 
             // tb_employeeid
             // 
-            this.tb_employeeid.Location = new System.Drawing.Point(110, 9);
+            this.tb_employeeid.Location = new System.Drawing.Point(108, 21);
             this.tb_employeeid.Name = "tb_employeeid";
-            this.tb_employeeid.Size = new System.Drawing.Size(171, 22);
+            this.tb_employeeid.Size = new System.Drawing.Size(158, 22);
             this.tb_employeeid.TabIndex = 3;
             // 
             // tb_fullname
             // 
-            this.tb_fullname.Location = new System.Drawing.Point(110, 37);
+            this.tb_fullname.Location = new System.Drawing.Point(108, 49);
             this.tb_fullname.Name = "tb_fullname";
-            this.tb_fullname.Size = new System.Drawing.Size(171, 22);
+            this.tb_fullname.Size = new System.Drawing.Size(158, 22);
             this.tb_fullname.TabIndex = 4;
             // 
             // tb_companyemail
             // 
-            this.tb_companyemail.Location = new System.Drawing.Point(110, 65);
+            this.tb_companyemail.Location = new System.Drawing.Point(108, 77);
             this.tb_companyemail.Name = "tb_companyemail";
-            this.tb_companyemail.Size = new System.Drawing.Size(171, 22);
+            this.tb_companyemail.Size = new System.Drawing.Size(158, 22);
             this.tb_companyemail.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(37, 40);
+            this.label3.Location = new System.Drawing.Point(39, 52);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 14);
             this.label3.TabIndex = 7;
@@ -99,40 +109,104 @@
             // 
             // tb_supemail
             // 
-            this.tb_supemail.Location = new System.Drawing.Point(110, 93);
+            this.tb_supemail.Location = new System.Drawing.Point(108, 105);
             this.tb_supemail.Name = "tb_supemail";
-            this.tb_supemail.Size = new System.Drawing.Size(171, 22);
+            this.tb_supemail.Size = new System.Drawing.Size(158, 22);
             this.tb_supemail.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 96);
+            this.label4.Location = new System.Drawing.Point(4, 108);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 14);
             this.label4.TabIndex = 9;
             this.label4.Text = "Supervisor Email:";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // adobebrowser
+            // 
+            this.adobebrowser.Dock = System.Windows.Forms.DockStyle.Right;
+            this.adobebrowser.Enabled = true;
+            this.adobebrowser.Location = new System.Drawing.Point(281, 0);
+            this.adobebrowser.Name = "adobebrowser";
+            this.adobebrowser.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("adobebrowser.OcxState")));
+            this.adobebrowser.Size = new System.Drawing.Size(490, 475);
+            this.adobebrowser.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(3, 177);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 54);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Save Location:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.b_generate);
+            this.groupBox2.Controls.Add(this.tb_employeeid);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tb_supemail);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tb_fullname);
+            this.groupBox2.Controls.Add(this.tb_companyemail);
+            this.groupBox2.Location = new System.Drawing.Point(3, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(272, 171);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Creation:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 175);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.tb_supemail);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.tb_companyemail);
-            this.Controls.Add(this.tb_fullname);
-            this.Controls.Add(this.tb_employeeid);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.b_generate);
+            this.ClientSize = new System.Drawing.Size(771, 475);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.adobebrowser);
             this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.Text = "Form Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.adobebrowser)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -147,6 +221,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox tb_supemail;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private AxAcroPDFLib.AxAcroPDF adobebrowser;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- Submission/Form1.cs
+++ Submission/Form1.cs
@@ -15,6 +15,12 @@ namespace Submission
 {
     public partial class Form1 : Form
     {
+
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        private static extern long ShellExecute(Int32 hWnd, string lpOperation,
+                                    string lpFile, string lpParameters,
+                                        string lpDirectory, long nShowCmd);
+
         public Form1()
         {
             InitializeComponent();
@@ -23,16 +29,18 @@ namespace Submission
         private void b_generate_Click(object sender, EventArgs e)
         {
             Document doc = new Document();
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
 
             PdfWriter.GetInstance(doc, new FileStream(String.Format("C:/{0}.pdf", tb_employeeid.Text), FileMode.Create));
             doc.Open();
 
-            Paragraph p1 = new Paragraph(String.Format("Employee ID: {0}",tb_employeeid.Text));
+            Paragraph p1 = new Paragraph(String.Format("Employee ID: {0}\n   Name: {1}", tb_employeeid.Text,tb_fullname.Text));
             doc.Add(p1);
             doc.Close();
-            MessageBox.Show("");
+            adobebrowser.src = String.Format("C:/{0}.pdf", tb_employeeid.Text);
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
