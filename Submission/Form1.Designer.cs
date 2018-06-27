0bdf906b424a0c8a259c40a1bfd87cb5ca799cfb
William Avery <willaveryvy@gmail.com>
2018-06-27 01:32:46 -0400

    asdf

--- Submission/Form1.Designer.cs
+++ Submission/Form1.Designer.cs
@@ -28,33 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.b_generate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_employeeid = new System.Windows.Forms.TextBox();
+            this.tb_fullname = new System.Windows.Forms.TextBox();
+            this.tb_companyemail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_supemail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // b_generate
             // 
-            this.button1.Location = new System.Drawing.Point(58, 26);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.b_generate.Location = new System.Drawing.Point(110, 138);
+            this.b_generate.Name = "b_generate";
+            this.b_generate.Size = new System.Drawing.Size(87, 25);
+            this.b_generate.TabIndex = 0;
+            this.b_generate.Text = "Generate";
+            this.b_generate.UseVisualStyleBackColor = true;
+            this.b_generate.Click += new System.EventHandler(this.b_generate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Company Email:";
+            // 
+            // tb_employeeid
+            // 
+            this.tb_employeeid.Location = new System.Drawing.Point(110, 9);
+            this.tb_employeeid.Name = "tb_employeeid";
+            this.tb_employeeid.Size = new System.Drawing.Size(171, 22);
+            this.tb_employeeid.TabIndex = 3;
+            // 
+            // tb_fullname
+            // 
+            this.tb_fullname.Location = new System.Drawing.Point(110, 37);
+            this.tb_fullname.Name = "tb_fullname";
+            this.tb_fullname.Size = new System.Drawing.Size(171, 22);
+            this.tb_fullname.TabIndex = 4;
+            // 
+            // tb_companyemail
+            // 
+            this.tb_companyemail.Location = new System.Drawing.Point(110, 65);
+            this.tb_companyemail.Name = "tb_companyemail";
+            this.tb_companyemail.Size = new System.Drawing.Size(171, 22);
+            this.tb_companyemail.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Full Name:";
+            // 
+            // tb_supemail
+            // 
+            this.tb_supemail.Location = new System.Drawing.Point(110, 93);
+            this.tb_supemail.Name = "tb_supemail";
+            this.tb_supemail.Size = new System.Drawing.Size(171, 22);
+            this.tb_supemail.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 14);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Supervisor Email:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(290, 175);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_supemail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_companyemail);
+            this.Controls.Add(this.tb_fullname);
+            this.Controls.Add(this.tb_employeeid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_generate);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Form Creator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button b_generate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_employeeid;
+        private System.Windows.Forms.TextBox tb_fullname;
+        private System.Windows.Forms.TextBox tb_companyemail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_supemail;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Submission/Form1.cs
+++ Submission/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace Submission
 {
@@ -15,6 +18,22 @@ namespace Submission
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void b_generate_Click(object sender, EventArgs e)
+        {
+            Document doc = new Document();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            PdfWriter.GetInstance(doc, new FileStream(String.Format("C:/{0}.pdf", tb_employeeid.Text), FileMode.Create));
+            doc.Open();
+
+            Paragraph p1 = new Paragraph(String.Format("Employee ID: {0}",tb_employeeid.Text));
+            doc.Add(p1);
+            doc.Close();
+            MessageBox.Show("");
+
+
         }
     }
 }
